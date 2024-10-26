7b0da318067778962d2cc619c2f6fbfd4593f6f0
BiaBicalho <beatrizbicalho2@gmail.com>
2024-10-25 21:38:08 -0300

    update com questões 4 e 5

--- JS/revisao/script.js
+++ JS/revisao/script.js
@@ -1,4 +1,65 @@
-let senha = 0000 //number
+// ATIVIDADES
+
+//Atividade 1: Verificação dia e noite
+let dia = true
+
+if(dia==true){
+   console.log("Está de dia")
+}else{
+   console.log("Está de noite")
+}
+
+//Atividade 2:Verificação de temperatura
+let temperatura = 25
+
+if(temperatura > 30){
+    console.log("Está muito quente")
+}else{
+    console.log("Está agradável a temperatura")
+}
+
+//Atividade 3: Verificação de faixa étaria
+
+let idade = 21
+
+if(idade < 12){
+    console.log("Criança")
+}else if(idade <= 18){
+    console.log("Adolescente")
+}else if(idade <= 60){
+    console.log("Adulto")
+}else{
+    console.log("Idoso")
+}
+
+//Atividade 4: Verificação de Senha
+
+let senha = 123
+
+let confirmacao = 125
+
+if(senha == confirmacao){
+    console.log("Senha Correta")
+}else{
+    console.log("Senhas não coincidem. Tente Novamente")
+}
+
+//Atividade 5: Calculadora de Notas
+
+let nota = 7.5
+
+if(nota >= 9){
+    console.log("Nota A")
+}else if(nota >= 8){
+    console.log("Nota B")
+}else if(nota >= 7){
+    console.log("Nota C")
+}else if(nota >= 6){
+    console.log("Nota D")
+}else{
+    console.log("Nota F")
+}
+/*let senha = 0000 //number
 
 let nome = "Fulano"//string
 
@@ -22,7 +83,7 @@ const termoDeUso = true // boolena
  >= se o valor é maior ou igual a outro
  <= se o valor é menor ou igual a outro
  */
-
+/*
  let number = "2"
 
  console.log(number == 2)
@@ -32,42 +93,11 @@ const termoDeUso = true // boolena
  if(condição){
     console.log("retorno")
  }
-*/
+*//*
  let totalCompra = prompt("Digite o valor da sua compra")
 
  if(totalCompra >= 20){
      alert("Frete Grátis!!!")
  }else{
      alert("Precisa pagar frete")
-}
-//Atividade 1 - Criar condição composta por uma variável dia e verifica se está de dia ou não
-let dia = true
-
-if(dia==true){
-   console.log("Está de dia")
-}else{
-   console.log("Está de noite")
-}
-
-//Atividade 2:Verifica temperatura, se > 30 ("Está muito quente"), do contrário ("Está muito agradável a temperatura")
-let temperatura = 25
-
-if(temperatura > 30){
-    console.log("Está muito quente")
-}else{
-    console.log("Está agradável a temperatura")
-}
-
-//Atividade 3: Verificação de faixa étaria, se < 12 (Criança), se entre 12 e 18 (adolsecente), sr entre 19 e 60 (Adulto), se a partir de 61 (Idoso)
-let idade = 21
-
-if(idade < 12){
-    console.log("Criança")
-}else if(idade <= 18){
-    console.log("Adolescente")
-}else if(idade <= 60){
-    console.log("Adulto")
-}else{
-    console.log("Idoso")
-}
-
+}*/
